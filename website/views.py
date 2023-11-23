567e89211cb8b7aab26fd3e94633f04af98a628a
Fuad Eyuboglu <fuadeyuboglu@gmail.com>
2023-11-23 20:33:45 +0000

    Added logic to home view

--- website/views.py
+++ website/views.py
@@ -3,7 +3,21 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    # Check to see if loggin in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Loggin In, Please Try Again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
 
 def logout_user(request):
     pass
